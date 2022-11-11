b744d50bde36cec1fc308a895b65d4bfb791635f
KyGuy2002 <kylanjaeger@gmail.com>
2022-11-11 12:39:58 -0800

    fix2

--- .eleventy.js
+++ .eleventy.js
@@ -1,27 +1,6 @@
-// Don’t forget to `npm install sass`!
-const sass = require("sass");
+
 
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addTemplateFormats("scss");
-
-  // Creates the extension for use
-  eleventyConfig.addExtension("scss", {
-    outputFileExtension: "css", // optional, default: "html"
-
-    // `compile` is called once per .scss file in the input directory
-    compile: async function(inputContent) {
-      let result = sass.compileString(inputContent);
-
-      // This is the render function, `data` is the full data cascade
-      return async (data) => {
-        return result.css;
-      };
-    }
-  });
-
-
-
-
   eleventyConfig.addPassthroughCopy('css')
   eleventyConfig.addPassthroughCopy('assets')
   eleventyConfig.addPassthroughCopy('js')
